cf6726f4b8ab32f9c3b830dd616e9cb282a15f62
joaovitorlemos <joaovitorlemos2015@gmail.com>
2023-06-30 15:33:02 -0300

    feat: features for routes and error handling

--- src/server.js
+++ src/server.js
@@ -1,12 +1,33 @@
+require("express-async-errors")
+
 const express = require("express")
-const app = express()
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
+const routes = require("./routes")
+const { request } = require("http")
 
+const app = express()
+app.use(express.json())
 
 migrationsRun()
 
+app.use(routes)
 
+app.use((error, request, response, next) => {
+   if(error instanceof AppError) {
+      return response.status(error.statusCode).json({
+         status: "error",
+         message: error.message
+      })
+   }
+
+   console.error(error)
+
+   return response.status(500).json({
+      status: "error",
+      message: "Internal server error"
+   })
+})
 
 
 
