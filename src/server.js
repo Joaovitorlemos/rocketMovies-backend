249569b9fe5a685286ccbc02f5457dd792022c6d
joaovitorlemos <joaovitorlemos2015@gmail.com>
2023-06-30 14:02:48 -0300

    import AppError to server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 const express = require("express")
 const app = express()
-
 const migrationsRun = require("./database/sqlite/migrations")
+const AppError = require("./utils/AppError")
 
 
 migrationsRun()
 
+
+
+
+
 const PORT = 3000
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
-
-app.get('/', function(require, response) {
-   response.send('Hello')
-})
