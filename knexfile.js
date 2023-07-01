0d7afa2ebc8b7c5ce763a605476f199067010fd4
joaovitorlemos <joaovitorlemos2015@gmail.com>
2023-06-30 21:17:27 -0300

    feat: adding new features

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require("path")
 
 module.exports = {
 
@@ -6,7 +6,17 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+
+    pool: {
+      afterCreate: (connection, callback) => connection.run("PRAGMA foreign_keys = ON", callback)
+    },
+
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     }
   },
+
+  useNullAsDefault: true
 
 };
